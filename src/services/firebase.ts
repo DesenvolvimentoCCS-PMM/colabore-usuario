a474f91d296e5020634d8c055fa2c3532b204ca6
Luan Henrique <luanhenrique.devvweb@gmail.com>
2024-12-06 13:43:09 -0300

    feat: add analytics

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
 export { db, auth };
